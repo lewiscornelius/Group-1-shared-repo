37ed485b28c1807a2583f6d71581d3c557e664a9
Brian12345-webdev <132722257+Brian12345-webdev@users.noreply.github.com>
2023-09-28 20:21:32 -0500

    update to fetch function

--- assets/index.js
+++ assets/index.js
@@ -8,14 +8,14 @@ function weatherForecast() {
  })
     .then(function (data) {
         console.log(data);
-    });
+    })
 
 
  fetch('http://api.weatherapi.com/v1/forecast.json?key=0326e1253a344fc8858235651232809&days=7')
 .then(function (response) {
     return response.json();
-});  
+})
     .then(function (data) {
         console.log(data)
-    });
+    })
  }
