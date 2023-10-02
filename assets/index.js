d9a4db84c195517e691a55a92986ba8f5b7b87f2
Brian12345-webdev <132722257+Brian12345-webdev@users.noreply.github.com>
2023-10-02 18:37:21 -0500

    fix error from last commit

--- assets/index.js
+++ assets/index.js
@@ -1,36 +1,38 @@
 var sevenDayForecast = document.querySelector('sevenDayContainer');
-
+var city = 
 
 function currentWeatherForecast() {
     fetch('http://api.weatherapi.com/v1/current.json?key=0326e1253a344fc8858235651232809')
 
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (data) {
-            this.displayWeather(data);
-        })
+ .then(function(response){
+    return response.json();
+
+ })
+    .then(function(data){
+        console.log(data);
+    });
 
 }
-function sevenDayForecast() {
-    fetch('http://api.weatherapi.com/v1/forecast.json?key=0326e1253a344fc8858235651232809&days=7')
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (data) {
-            console.log(data)
-        });
+function sevenDayForecast(){
+fetch('http://api.weatherapi.com/v1/forecast.json?key=0326e1253a344fc8858235651232809&days=7')
+.then(function (response) {
+   return response.json();
+})
+   .then(function (data) {
+       console.log(data)
+   });
+
 }
 
-function search() {
-    this.sevenDayForecast(document.querySelector('.input').value);
-};
+function trafficReport(){
+fetch("https://api.tomtom.com/traffic/services/4/flowSegmentData/absolute/10/xml?key={kYLbyqIA0g1ERQMZT87Gd66qOcgrH7nT}&point=52.41072,4.84239")
+ 
+.then(function(response){
+   return response.json();
+})
+   .then(function(data){
+       console.log(data);
+   });
+  
 
-document.querySelector('.input').addEventListener('keyup', function (event)) {
-    if (event.key == 'Enter') {
-        sevenDayForecast.search();
-        document.querySelector('.input').value;
-        sevenDayForecast(document.querySelector('.input').value);
-    }
-    document.querySelector('.input').
 }
