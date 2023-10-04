18750a57dec1b610e740907e880423a1473ab685
Jacob Fratti <JacobRFratti@gmail.com>
2023-10-04 13:50:13 -0400

    Still working on mapquest

--- assets/index.js
+++ assets/index.js
@@ -1,5 +1,5 @@
 var sevenDayForecast = document.querySelector('sevenDayContainer');
-var city = {lng: -122, lat: 37}; //place holder
+var city = 'Orlando'; //place holder
 
 function currentWeatherForecast() {
     fetch('http://api.weatherapi.com/v1/current.json?key=0326e1253a344fc8858235651232809')
@@ -24,25 +24,34 @@ fetch('http://api.weatherapi.com/v1/forecast.json?key=0326e1253a344fc88582356512
 
 }
 
-// // point = is the lat and long for coordinates
-// function trafficReport(){
-// fetch("https://api.tomtom.com/traffic/services/4/flowSegmentData/relative0/10/json?point=52.41072%2C4.84239&unit=MPH&openLr=false&key=DzuYR8JOWdmvyTeedltrKg0WXzWiKulJ")
- 
-// .then(function(response){
-//    return response.json();
-// })
-//    .then(function(data){
-//        console.log(data);
-//    });
-// }
+// Initialize the map
+var map = L.map('map').setView([0, 0], 13); // Set initial coordinates and zoom level
 
-// Map Display
-var trafficKey = 'DzuYR8JOWdmvyTeedltrKg0WXzWiKulJ'
+// Create a MapQuest tile layer
+L.tileLayer('https://open.mapquestapi.com/tiles/layer/{mapId}/{z}/{x}/{y}.png?key=gUgK3zuO2juaY9exckXhIafpgj38trkj', {
+    attribution: '© <a href="https://www.mapquest.com/">MapQuest</a>',
+    maxZoom: 18,
+    style: 'osm' // You can choose a different style here
+}).addTo(map);
 
-var map = tt.map({
-    key: trafficKey,
-    container: 'map-div',
-    center: city,
-    zoom: 12,
-    style: 'https://api.tomtom.com/style/2/custom/style/dG9tdG9tQEBAVmV2cTdUdHloQlR4b2J5Ujs5ZWRmZTk2ZC1jNjI5LTQ3YWUtODY4OS0yNDA2NGJiZTQ4ZGY=.json?key=DzuYR8JOWdmvyTeedltrKg0WXzWiKulJ'
-})
+// Function to perform the map search
+function searchMap() {
+
+    // Use MapQuest's free geocoding API to convert the location to coordinates
+    fetch(`https://www.mapquestapi.com/geocoding/v1/address?key=gUgK3zuO2juaY9exckXhIafpgj38trkj&location=${city}`)
+        .then(response => response.json())
+        .then(data => {
+            if (data.results.length > 0) {
+                var coordinates = data.results[0].locations[0].latLng; // Get the coordinates from the API response
+                map.setView([coordinates.lat, coordinates.lng], 13); // Set the map view to the searched location
+                L.marker([coordinates.lat, coordinates.lng]).addTo(map); // Add a marker at the searched location
+            } else {
+                alert('Location not found. Please try again.');
+            }
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+}
+
+
