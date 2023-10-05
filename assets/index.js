ef5a4ca427c0646ec237d02132b62d097dd0c2a6
lewiscornelius <lewiscornelius7@gmail.com>
2023-10-04 20:02:12 -0400

    yes

--- assets/index.js
+++ assets/index.js
@@ -1,16 +1,84 @@
-var sevenDayForecast = document.querySelector('sevenDayContainer');
+var sevenDayForecastEl = document.querySelector('#sevenDayContainer');
+var currentTempEl = document.querySelector('#currentTemp');
+var currentWeatherEl = document.querySelector('#currentWeather');
 
-function weatherForecast() {
- fetch('http://api.weatherapi.com/v1/current.json?key=0326e1253a344fc8858235651232809')
+var city = { lng: -122, lat: 37 }; //place holder
 
- .then(function(response){
-    return response.json();
- })
-    .then(function(data){
-        console.log(data);
+function currentWeatherForecast() {
+    fetch('http://api.weatherapi.com/v1/current.json?key=0326e1253a344fc8858235651232809')
+
+        .then(function (response) {
+            return response.json();
+
+        })
+        .then(function (data) {
+            console.log(data);
+        });
+
+}
+function sevenDayForecast() {
+    fetch('http://api.weatherapi.com/v1/forecast.json?key=0326e1253a344fc8858235651232809&days=7')
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data)
+        });
+}
+
+function searchInput() {
+    this.sevenDayForecast(document.getElementById(".input").value);
+    console.log(value);
+}
+
+document.querySelector(".input").addEventListener("keydown", function (event) { 
+    if (event.key === "Enter") {
+        event.preventDefault(); // Prevent form submission or page reload
+        var inputValue = document.querySelector(".input").value;
+        sevenDayForecast.search(inputValue);
+    }
+});
+
+
+
+function displaySevenDayForecast(forecast) {
+    for (var i = 0; i < 7; i++) {
+        var sevenDays = document.querySelector('span');
+        var weather = document.createElement('div');
+        weather.classList = 'list-item flex-row justify-space-between align-center';
+
+        var temperature
+
+        weather.appendChild(sevenDays)
+        sevenDayForecastEl.appendChild(weather);
+    }
+}
+
+// // Initialize the map
+window.onload = function() {
+    L.map('mapid', {
+      layers: MQ.mapLayer(),
+      center: [ 40.731701, -73.993411 ],
+      zoom: 12
     });
+  };
 
-
- fetch('http://api.weatherapi.com/v1/forecast.json?key=0326e1253a344fc8858235651232809&days=7')
-
- }
+//This function needs to be ran on search
+// Function to perform the map search
+function searchMap() {
+    // Use MapQuest's free geocoding API to convert the location to coordinates
+    fetch(`https://www.mapquestapi.com/geocoding/v1/address?key=gUgK3zuO2juaY9exckXhIafpgj38trkj&location=${city}`)
+        .then(response => response.json())
+        .then(data => {
+            if (data.results.length > 0) {
+                var coordinates = data.results[0].locations[0].latLng; // Get the coordinates from the API response
+                map.setView([coordinates.lat, coordinates.lng], 13); // Set the map view to the searched location
+                L.marker([coordinates.lat, coordinates.lng]).addTo(map); // Add a marker at the searched location
+            } else {
+                alert('Location not found. Please try again.');
+            }
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+    }
